dd877875ec63bb2e4c2bcf30c5a488fe37cbbb78
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-01-14 21:06:31 +0800

    Implementing Delete def function

--- Binary Search Tree - PART 2/BT_Part2.py
+++ Binary Search Tree - PART 2/BT_Part2.py
@@ -68,3 +68,7 @@ class BinarySearchTreeNode:
             elements += self.right.in_order_traversal() # ' ' ' '
             
         return elements # it returns all the elements in the tree in specific order [ascending order]
+    
+    # TODO: Implementation of delete def function
+    def delete(self, val): # implementing delete function where we can supply a particular value and it will delete the node from the binary tree
+        if val < self.data: # checking if the value is less than the self.data
