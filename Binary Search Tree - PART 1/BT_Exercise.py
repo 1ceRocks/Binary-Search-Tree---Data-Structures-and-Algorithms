3ef1960b75e4f88c5728d46f4af2d6cbb43fe582
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-01-14 19:53:16 +0800

    Simulating In Order Traversal & Customizing Print

--- Binary Search Tree - PART 1/BT_Exercise.py
+++ Binary Search Tree - PART 1/BT_Exercise.py
@@ -134,5 +134,6 @@ if __name__ == "__main__":
     print("Minimum number: ", numbers_tree.find_min())
     print("Maximum number: ", numbers_tree.find_max())
     print("Calculated sum: ", numbers_tree.calculate_sum())
-    print("\nPre Order Traversal: ", numbers_tree.pre_order_traversal())
-    print("Post Order Traversal: ", numbers_tree.post_order_traversal())
+    print("\nIn Order Traversal: ", numbers_tree.in_order_traversal())
+    print("Pre Order Traversal: ", numbers_tree.pre_order_traversal())
+    print("Post Order Traversal: ", numbers_tree.post_order_traversal(), "\n")
