d4125ae3db401c211161e1955c485cdae0709283
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-01-14 22:05:52 +0800

    Outsourcing the Needed Output for the Demo

---
+++ Full-Name_BT.py
@@ -0,0 +1,13 @@
+# PYTHON BINARY SEARCH TREE
+# DATA STRUCTURES AND ALGORITHMS
+# BSCOE 2-6 (2022-2023) -- GITHUB
+# 1/14/2023
+
+"""
+    Source Code: https_://www.youtube.com/watch?v=lFq5mYUWEBk
+                 https_://www.youtube.com/watch?v=JnrbMQyGLiU
+                 
+    Create a demo using the letters in your fullname as the content of the binary tree.
+Upload all source code in new github repository.
+"""
+
