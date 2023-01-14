fff27a34a70133198d0647d36eade01c812143f3
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-01-14 22:10:15 +0800

    def add_child(self, data) ProgramCode Simulated

--- Full-Name_BT.py
+++ Full-Name_BT.py
@@ -16,3 +16,21 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+        
+    def add_child(self, data):
+        if data ==self.data:
+            return # binary search tree cannot have duplicate children or elements which are not equal to the current node
+        
+        if data < self.data:
+            # add data in left subtree;
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+            
+        else:
+            # add data in right subtree;
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
