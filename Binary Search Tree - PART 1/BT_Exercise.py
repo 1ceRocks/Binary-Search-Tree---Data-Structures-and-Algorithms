06c1a9ed4ed092c1c1626a9612ce099b267c4c73
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-01-14 18:58:06 +0800

    Initializing the def function for find_min(self)
    We will now create a program code block for finding the minimum element using self as a parameter in our binary tree entirety.

--- Binary Search Tree - PART 1/BT_Exercise.py
+++ Binary Search Tree - PART 1/BT_Exercise.py
@@ -70,6 +70,9 @@ class BinarySearchTreeNode:
             
         return elements # it returns all the elements in the tree in specific order [ascending order]
     
+    # TODO: We need to create a def function for finding the minimum order of precedence for a given data node or element in our entire binary tree
+    def find_min(self):
+    
 def build_tree(elements):
     print("\nBuilding tree with these elements: ", elements)
     # root node for the tree element
